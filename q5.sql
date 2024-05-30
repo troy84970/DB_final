b2b73f0ec215ccbdf59206646f7333b398709ea5
troy84970 <troy84970@gmail.com>
2024-05-30 14:55:46 +0800

    Update q5.sql

--- q5.sql
+++ q5.sql
@@ -31,25 +31,73 @@ on c.course_arrangement_id = cs.course_arrangement_id and c.student_id = cs.stud
 where course_score >= 60;
 */
 
-SELECT 
+/*
+select 
     c.course_arrangement_id,
     count(*) AS total_students,
     SUM(CASE WHEN cs.course_score < 70 THEN 1 ELSE 0 END) AS failed_students
 from master_student_with_selected_course AS c
 join course_score as cs
 on c.course_arrangement_id = cs.course_arrangement_id AND c.student_id = cs.student_id
-group by c.course_arrangement_id;
-
-SELECT 
+group by c.course_arrangement_id
+order by course_arrangement_id;
+*/
+/*
+select
     c.course_arrangement_id,
     count(*) AS total_students,
     SUM(CASE WHEN cs.course_score < 70 THEN 1 ELSE 0 END) AS failed_students
 from college_student_with_selected_course AS c
 join course_score as cs
 on c.course_arrangement_id = cs.course_arrangement_id AND c.student_id = cs.student_id
-group by c.course_arrangement_id;
+group by c.course_arrangement_id
+order by course_arrangement_id;
+*/
+
+create view course_score_distribution as
+select 
+    course_arrangement_id,
+    SUM(total_students) as total_students,
+    SUM(failed_students) as failed_students
+from (
+    (select
+        c.course_arrangement_id,
+        COUNT(*) as total_students,
+        SUM(CASE WHEN cs.course_score < 70 THEN 1 ELSE 0 END) AS failed_students
+    from master_student_with_selected_course AS c
+    join course_score AS cs ON c.course_arrangement_id = cs.course_arrangement_id AND c.student_id = cs.student_id
+    group by c.course_arrangement_id) 
+    union all 
+    (select
+        c2.course_arrangement_id,
+        COUNT(*) AS total_students,
+        SUM(CASE WHEN cs2.course_score < 70 THEN 1 ELSE 0 END) AS failed_students
+    from college_student_with_selected_course AS c2
+    join course_score AS cs2 ON c2.course_arrangement_id = cs2.course_arrangement_id AND c2.student_id = cs2.student_id
+    group by c2.course_arrangement_id)
+) as combined_data
+group by course_arrangement_id;
+
+/*
+select * from course_score_distribution
+order by course_arrangement_id;
+*/
+
+-- final result
+select c.course_name,t.teacher_name, csd.failed_students,csd.total_students,csd.failed_students/csd.total_students as failure_rate
+from course_score_distribution as csd
+join course_arrangement as ca
+on ca.arrangement_id = csd.course_arrangement_id
+join teach 
+on teach.course_arrangement_id = csd.course_arrangement_id
+join teacher as t
+on t.teacher_id = teach.teacher_id
+join course as c
+on c.course_no = ca.course_no;
+    
 
 
 drop view student_with_selected_course;
 drop view master_student_with_selected_course;
 drop view college_student_with_selected_course;
+drop view course_score_distribution;
