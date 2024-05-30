107afa8f178d474ca56d4f84e7f804b1bb0e8511
troy84970 <troy84970@gmail.com>
2024-05-30 13:49:38 +0800

    Update q5.sql

--- q5.sql
+++ q5.sql
@@ -17,10 +17,17 @@ from student_with_selected_course
 where student_dept not like '%研究所' and student_dept not like '%碩士班';
 
 
-select * 
+select c.course_arrangement_id,c.student_id,course_score
 from master_student_with_selected_course as c
 join course_score as cs
-on c.course_arrangement_id = cs.course_arrangement_id and c.student_id = cs.student_id;
+on c.course_arrangement_id = cs.course_arrangement_id and c.student_id = cs.student_id
+where course_score >= 70;
+
+select c.course_arrangement_id,c.student_id,course_score
+from college_student_with_selected_course as c
+join course_score as cs
+on c.course_arrangement_id = cs.course_arrangement_id and c.student_id = cs.student_id
+where course_score >= 60;
 
 drop view student_with_selected_course;
 drop view master_student_with_selected_course;
