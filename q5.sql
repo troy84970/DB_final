da846bffbeba6f36efe8fbf83d3ed02573901c79
troy84970 <troy84970@gmail.com>
2024-06-06 01:16:02 +0800

    Create q5.sql

---
+++ q5.sql
@@ -0,0 +1,20 @@
+with course_feedback_analyze as (
+	select ca.arrangement_id,c.course_no,sum(cf.feedback_rank) as total_feedback,avg(cf.feedback_rank) as avg_feedback
+	from course_arrangement as ca 
+		join enrollment as e on ca.arrangement_id = e.course_arrangement_id
+		join student as s on s.student_id = e.student_id
+		join course as c on c.course_no = ca.course_no
+		join course_feedback as cf on cf.student_id = s.student_id and cf.course_arrangement_id = ca.arrangement_id
+	where e.select_result = '中選' and s.student_status = '在學'
+	group by c.course_no,ca.arrangement_id
+)
+select course_no,course_name,group_concat(teacher_name) as teacher_name,total_feedback,avg_feedback
+from (
+	select cfa.course_no,c.course_name,t.teacher_name,cfa.total_feedback, cfa.avg_feedback
+	from course_feedback_analyze as cfa
+		join course as c on cfa.course_no = c.course_no
+		join teach on teach.course_arrangement_id = cfa.arrangement_id
+		join teacher as t on t.teacher_id = teach.teacher_id
+    ) as normalize_result
+group by course_no,course_name,total_feedback,avg_feedback
+order by avg_feedback desc;
